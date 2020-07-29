71b51ce8b23076840d96368b024f07346eacbf75
Matt Mason <masonmj10@gmail.com>
2020-07-29 13:22:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog 
+<<<<<<< HEAD
   def name=(name)
     @name = name
   end
@@ -13,7 +14,32 @@ class Dog
   
   def breed
     @breed
+=======
+  def name=(dogs_name)
+    @doggy_name = dogs_name
+  end
+  
+  def name
+    @doggy_name
+  end 
+  
+  def breed=(dog_breed)
+    @the_dogs_breed = dog_breed
+  end
+  
+  def breed
+    @the_dogs_breed
+>>>>>>> a409af6067269cc46cdd425883b0320c4f41dcc1
   end
   
 end
 
+<<<<<<< HEAD
+=======
+fido = Dog.new 
+fido.name = "Fido"
+
+
+puts fido.name 
+
+>>>>>>> a409af6067269cc46cdd425883b0320c4f41dcc1
